ba98ad45089626c025384a00fe04d6e57e486482
sauravmondal24 <sauravmondal321@gmail.com>
2022-10-25 01:58:26 +0600

    Client side project setup

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyD59hDYXspGJZPdRCd-Sv_HjvtYK8ErKvU',
+	authDomain: 'eduon-b1031.firebaseapp.com',
+	projectId: 'eduon-b1031',
+	storageBucket: 'eduon-b1031.appspot.com',
+	messagingSenderId: '563867242041',
+	appId: '1:563867242041:web:665743f3a1c3a88207c7e8'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
