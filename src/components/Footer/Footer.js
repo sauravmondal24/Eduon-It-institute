d61a88fe53e07e204cc9ea9ba07fbc90d9686b84
sauravmondal24 <sauravmondal321@gmail.com>
2022-10-26 10:39:57 +0600

    Footer section done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/img/logo.png';
 
 const Footer = () => {
 	return (
-		<div>
-			<h2>This is a Footer section</h2>
-		</div>
+		<footer className="footer p-10 bg-base-200 text-base-content ">
+			<div>
+				<img src={logo} alt="" />
+				<p>Eduon It institute. Providing reliable tech since 1992</p>
+			</div>
+			<div>
+				<span className="footer-title font-bold">Services</span>
+				<Link className="link link-hover">Branding</Link>
+				<Link className="link link-hover">Design</Link>
+				<Link className="link link-hover">Marketing</Link>
+				<Link className="link link-hover">Advertisement</Link>
+			</div>
+			<div>
+				<span className="footer-title font-bold">Company</span>
+				<Link className="link link-hover">About us</Link>
+				<Link className="link link-hover">Contact</Link>
+				<Link className="link link-hover">Jobs</Link>
+				<Link className="link link-hover">Press kit</Link>
+			</div>
+			<div>
+				<span className="footer-title font-bold text-black">Legal</span>
+				<Link className="link link-hover">Terms of use</Link>
+				<Link className="link link-hover">Privacy policy</Link>
+				<Link className="link link-hover">Cookie policy</Link>
+			</div>
+		</footer>
 	);
 };
 
